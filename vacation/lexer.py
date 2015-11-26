eb1f49b482a24a6a076732538ebeaea9ae93dcf2
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-11-26 12:53:50 -0800

    Use constants for valid syntax tokens

--- vacation/lexer.py
+++ vacation/lexer.py
@@ -1,4 +1,11 @@
-def lex(inputs):
+
+SHOW = 'show'
+TAKE = 'take'
+TAKERANGE = 'takerange'
+SETRATE = 'setrate'
+SETDAYS = 'setdays'
+MONTHS = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+
     """ Lex input. """
 
     print('inputs were {}'.format(inputs))
