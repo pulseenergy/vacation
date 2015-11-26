f9097cbc56d08c9e22a8291a038ba9cf4de3df57
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-11-26 14:05:18 -0800

    Extract multiple dates from args

--- vacation/lexer.py
+++ vacation/lexer.py
@@ -1,3 +1,4 @@
+import pdb
 
 SHOW = 'show'
 TAKE = 'take'
@@ -9,20 +10,34 @@ MONTHS = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 
 def lex(args):
     """ Lex input. """
     if len(args) == 0:
+        # Show user their vacation days
         return [(SHOW)]
     elif isMonth(args[0]):
-        if not args[2:]:
-            return [(TAKE, lexDate(args[0:2]))]
+        # Take one vacation day
+        ret = [(TAKE, date) for date in lexDate(args[0:])]
+        #print('\n@@@ {}'.format(ret))
+        return ret
 
 
 def lexDate(args):
-    #print('\n@@@ {}'.format(args))
     month = args[0][:3].lower()
-    day = int(args[1])
     if not isMonth(args[0]):
         raise ValueError('Not valid month')
-    return '{} {}'.format(month, day)
+
+    dates = []
+    for arg in args[1:]:
+        day = getDay(arg)
+        dates.append('{} {}'.format(month, day))
+
+    return dates
 
 def isMonth(arg):
     month = arg[:3].lower()
     return month in MONTHS
+
+def getDay(arg):
+    arg = arg.strip(',')
+    try:
+        return int(arg)
+    except ValueError:
+        raise ValueError('Invalid day: {}'.format(arg))
