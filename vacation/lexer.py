ed8c784ea787f83ddb1adf4640c72424569bb227
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-11-26 12:54:16 -0800

    Extract a single date from args

--- vacation/lexer.py
+++ vacation/lexer.py
@@ -6,6 +6,23 @@ SETRATE = 'setrate'
 SETDAYS = 'setdays'
 MONTHS = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
 
+def lex(args):
     """ Lex input. """
+    if len(args) == 0:
+        return [(SHOW)]
+    elif isMonth(args[0]):
+        if not args[2:]:
+            return [(TAKE, lexDate(args[0:2]))]
 
-    print('inputs were {}'.format(inputs))
+
+def lexDate(args):
+    #print('\n@@@ {}'.format(args))
+    month = args[0][:3].lower()
+    day = int(args[1])
+    if not isMonth(args[0]):
+        raise ValueError('Not valid month')
+    return '{} {}'.format(month, day)
+
+def isMonth(arg):
+    month = arg[:3].lower()
+    return month in MONTHS
