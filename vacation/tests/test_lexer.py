a29c3e81ee7d91c8f549895d98bb640b20277c67
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-11-26 12:56:12 -0800

    Rename test file to match lexer

---
+++ vacation/tests/test_lexer.py
@@ -0,0 +1,57 @@
+import lexer
+
+
+def test_no_args():
+    inputs = []
+    expected = [('show')]
+    assert lexer.lex(inputs) == expected
+
+def test_one_date():
+    inputs = ['Nov', '8']
+    expected = [('take', 'nov 8')]
+    assert lexer.lex(inputs) == expected
+
+def test_two_dates():
+    inputs = ['Nov', '8,', '12']
+    expected = [('take', 'nov 8'), ('take', 'nov 12')]
+    assert lexer.lex(inputs) == expected
+
+def test_date_easy_range():
+    inputs = ['Nov', '8-10']
+    expected = [('takerange', 'nov 8', 'nov 10')]
+    assert lexer.lex(inputs) == expected
+
+def test_date_months_range():
+    inputs = ['Nov', '8', '-', 'Dec', '2']
+    expected = [('takerange', 'nov 8', 'dec 2')]
+    assert lexer.lex(inputs) == expected
+
+def test_date_months_range_sub():
+    inputs = ['Nov', '8-Dec', 'Dec', '2']
+    expected = [('takerange', 'nov 8', 'dec 2')]
+    assert lexer.lex(inputs) == expected
+
+def test_date_crazy():
+    inputs = ['Nov30-Dec2']
+    expected = [('takerange', 'nov 30', 'dec 2')]
+    assert lexer.lex(inputs) == expected
+
+def test_set_rate():
+    inputs = ['set', 'rate', '1.333']
+    expected = [('setrate', '1.333')]
+    assert lexer.lex(inputs) == expected
+
+def test_set_days():
+    inputs = ['set', 'days', '10.5']
+    expected = [('setdays', '10.5')]
+    assert lexer.lex(inputs) == expected
+
+def test_manually_add_day():
+    inputs = ['adjust', '+1']
+    expected = [('adjust', '1')]
+    assert lexer.lex(inputs) == expected
+
+def test_manually_subtract_day():
+    inputs = ['adjust', '-1']
+    expected = [('adjust', '-1')]
+    assert lexer.lex(inputs) == expected
