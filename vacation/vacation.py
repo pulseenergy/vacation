1ed07698dc91a6c620036c8741e472b2138dff30
Mitch LeBlanc <mitch.leblanc@enernoc.com>
2015-11-26 11:03:16 -0800

    Add setup_args()

--- vacation/vacation.py
+++ vacation/vacation.py
@@ -1,8 +1,29 @@
+import argparse
+
+import parser
+
+
+def setup_args():
+    parser = argparse.ArgumentParser('Vacation Tracker')
+    parser.add_argument('input', nargs='*')
+    return parser.parse_args()
+
+
 def main():
-    print("you have 5 vacation days left")
+    args = setup_args()
+    print('Input args: {}'.format(args.input))
+
+    def tester():
+        print(args)
+    tester()
+    parser.parse(args.input)
+
+    print("5 vacation days remaining")
+
 
 def test():
     print("testing")
+
 
 if __name__ == '__main__':
     main()
