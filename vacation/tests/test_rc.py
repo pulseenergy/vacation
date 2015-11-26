46a19bc0dd6e7254bd0cb39196ef42ab24f72dce
Mitch LeBlanc <mitch.leblanc@enernoc.com>
2015-11-26 12:50:54 -0800

    Add some RC utils tests

---
+++ vacation/tests/test_rc.py
@@ -0,0 +1,18 @@
+import os
+
+import rc
+
+
+def test_rc_file():
+    temp = os.path.join(os.path.expanduser('~'), '.vacationrc')
+    assert rc.rc_file() == temp
+
+
+def test_touch_rc():
+    temp = os.path.join(os.path.expanduser('~'), '.vacationtestrc')
+    assert os.path.isfile(temp) is False
+    rc.touch_rc(temp)  # Should create file
+    assert os.path.isfile(temp) is True
+    os.unlink(temp)  # Cleanup
+    assert os.path.isfile(temp) is False
+
