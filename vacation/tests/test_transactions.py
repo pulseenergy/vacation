35f5479dc0889f559f464eecd78177599dba5be5
Mitch LeBlanc <supermitch@gmail.com>
2015-11-30 21:06:33 -0800

    Clean up test (2 failing)

--- vacation/tests/test_transactions.py
+++ vacation/tests/test_transactions.py
@@ -4,53 +4,73 @@ from nose.tools import assert_almost_equals
 
 from vacation import transactions as trans
 
-transactions = [
-    '2015-11-01: days 0',
-    '2015-11-01: rate 20',
-    '2015-11-10: off',
-    '2015-11-15: show',
-]
-
 
 def test_validate_setup_empty():
     assert trans.validate_setup([]) is True
 
 
 def test_validate_setup_missing_rate():
+    transactions = ['2015-11-01: days 0']
     assert trans.validate_setup(transactions[:1]) is False
 
 
 def test_validate_setup_good():
+    transactions = [
+        '2015-11-01: days 0',
+        '2015-11-01: rate 20',
+        '2015-11-10: off',
+        '2015-11-15: show',
+    ]
     assert trans.validate_setup(transactions) is True
 
 
 def test_validate_setup_bad_dates():
-    bad = transactions.copy()
-    bad[0] = '2015-11-02: days 0'  # Wrong date
-    print(bad)
+    transactions = [
+        '2015-11-02: days 0',  # Wrong date
+        '2015-11-01: rate 20',
+        '2015-11-10: off',
+        '2015-11-15: show',
+    ]
     assert trans.validate_setup(bad) is False
 
 
 def test_validate_setup_bad_missing_set():
-    bad = transactions.copy()
-    del bad[1]  # Missing rate
+    transactions = [  # Missing set rate
+        '2015-11-01: days 0',
+        '2015-11-10: off',
+        '2015-11-15: show',
+    ]
     assert trans.validate_setup(bad) is False
 
 
 def test_parse_transaction_entry_good_value():
-    entry = transactions[1]
+    entry = '2015-11-01: rate 20'
     expected = (datetime.date(2015, 11, 1), 'rate', 20.0)
     assert trans._parse_transaction_entry(entry) == expected
 
 
 def test_parse_transaction_entry_good_no_value():
-    entry = transactions[2]
+    entry = '2015-11-10: off'
     expected = (datetime.date(2015, 11, 10), 'off', None)
     assert trans._parse_transaction_entry(entry) == expected
 
 
 def test_sum_transactions_easy():
-    result = trans.sum_transactions(transactions)
-    expected = 15 * (20.0 / 250) - 1
-    assert_almost_equals(result, expected)
+    transactions = [  # 8 working days: 10 off & 11 is Holiday
+        '2015-11-01: days 0',
+        '2015-11-01: rate 20',
+        '2015-11-15: show',
+    ]
+    expected = 8 * (20.0 / 250)
+    assert_almost_equals(trans.sum_transactions(transactions), expected)
+
+
+def test_sum_transactions_set_days():
+    transactions = [
+        '2015-11-01: days 0',
+        '2015-11-01: rate 20',
+        '2015-11-10: off',
+        '2015-11-15: days 7',
+    ]
+    assert_almost_equals(trans.sum_transactions(transactions), 7)
 
